2a1e126605625e038a0b56e96b7dd3fa25b70538
Daniel Fang <daniel.bb0321@gmail.com>
2025-11-24 14:04:22 -0800

    set next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  productionBrowserSourceMaps: false,
+  swcMinify: true,
+  compress: true,
 };
 
 export default nextConfig;
