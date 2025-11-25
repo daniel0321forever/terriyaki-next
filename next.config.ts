e8414673907204666567f7dd0646b5f790874ce0
Daniel Fang <daniel.bb0321@gmail.com>
2025-11-24 23:31:22 -0800

    solve invalid next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   productionBrowserSourceMaps: false,
-  swcMinify: true,
   compress: true,
 };
 
